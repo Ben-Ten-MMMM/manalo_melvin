1c63e3ef7d353c6927d4079d180edec6f3bbe388
Melvin Manalo <manalomelvin023@gmail.com>
2025-09-18 12:41:53 +0800

    first commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
      'driver'    => 'mysql',
-    'hostname'  =>  getenv(' sql12.freesqldatabase.com'),
-    'port'      => getenv('3306'),
-    'username'  => getenv('sql12799045'),
-    'password'  => getenv(' 9v6PQbaM3c'),
-    'database'  => getenv('sql12799045'),
+    'hostname'  =>  getenv("DB_HOST"),
+    'port'      => getenv('DB_PORT'),
+    'username'  => getenv("DB_USER"),
+    'password'  => getenv("DB_PASSWORD"),
+    'database'  => getenv("DB_DATABASE"),
     'charset'   => 'utf8mb4',
     'dbprefix'  => '',
     'path'      => ''      
